413dc71086b05cf59b518a0d39243e06aae7e88c
Toomas <toomas.heljula@khk.ee>
2022-05-10 10:16:48 +0300

    Basic use of EJS

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const app = express();
 
 
 
-app.get('/', (req, res) => {
-    res.send('<a href="/contact">Contact Us</a> <br> <a href="/about">About Us</a>');
+app.get('/user/:username', (req, res) => {
+    let user = req.params.username;
+    res.render('index.ejs',{username : user});
 });
-app.get('/contact', (req, res) => {
-    res.send('<h1>Contact Us Page</h1>');
-});
-app.get('/about', (req, res) => {
-    res.send('<h1>About Us Page</h1>');
-});app.get('*', (req, res) => {
-    res.send('404. This page does not exist.<a href="/">Go to home page</a>');
-});
+
 
 app.listen(3000, () => {
     console.log(`Server started on http://localhost:3000`);
