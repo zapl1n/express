288641509eda35cc4346c7532ea18677b103fa6b
Toomas <toomas.heljula@khk.ee>
2022-05-10 11:05:37 +0300

    Post request use

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-app.get('/questions', (req, res) => {
-    let questions = [
-        {title:"What is node.js?", user : "Kadi", votes:"10"},
-        {title:"What is nexpress.js?", user : "Mike", votes:"8"}
-    ]
-    res.render('index',{questions : questions});
+app.get('/login', (req, res) => {
+
+    res.render('login');
 });
 
+app.post('/login', (req, res) => {
+let username = req.body.username;
+let password = req.body.password;
+
+if(username === 'user' && password ==='qwerty'){
+    res.redirect('/dashboard');
+}
+});
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
 
 app.listen(3000, () => {
     console.log(`Server started on http://localhost:3000`);
