a4b949dcb5cc5e639a5c5e9d48e921e06487f4b9
Toomas <toomas.heljula@khk.ee>
2022-05-10 10:25:07 +0300

    Public directory use

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 
 app.get('/user/:username', (req, res) => {
